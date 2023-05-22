4fd7933ca52272cd90718f2f3a05b18477518deb
kerthiyashwanthrao <kerthiyashwanthrao@gmail.com>
2023-05-22 14:15:45 +0000

    Authentication Functionality

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+// Write your JS code here
+import Header from '../Header'
+import LogoutButton from '../LogoutButton'
+
+const About = () => (
+  <>
+    <Header />
+    <div className="container">
+      <h1>About Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import Header from '../Header'
+import LogoutButton from '../LogoutButton'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="container">
+      <h1>Home Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+export default Home
